8b8a6d178aa0010d48840a7568e1a7e1a744da55
Ignacio Ramírez Paulino <nacho@fing.edu.uy>
2023-12-15 17:09:12 -0300

    ajustados parametros para toma 3 de salida 3

--- code/apply_calibration.py
+++ code/apply_calibration.py
@@ -12,6 +12,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from skimage import io as imgio
+from skimage import transform as trans
 
 if __name__ == "__main__":
 
@@ -25,7 +26,7 @@ if __name__ == "__main__":
                     help="First calibration frame.")
     ap.add_argument('-f',"--finish", type=int, default=10000000,
                     help="Last calibration frame.")
-    ap.add_argument('-r',"--skip", type=int, default=6,
+    ap.add_argument('-r',"--skip", type=int, default=15,
                     help="Save a frame every <skip> frames. Defaults to 1 (no skip).")
 
     ap.add_argument('-w',"--wcurve", type=str, required=True,
@@ -36,6 +37,7 @@ if __name__ == "__main__":
                     help="output prefix. ")
     ap.add_argument('-e',"--exposure", type=float, default=0.25,
                     help="Global exposure, between 0 and 1. Defaults to 0.25. ")
+    ap.add_argument('-R','--rotation',type=int,default=0,help="Optionally rotate the frames this many degrees clockwise (Defualts to 0).")
 
     args = vars(ap.parse_args())
 
@@ -47,7 +49,7 @@ if __name__ == "__main__":
     wbfile  = args["wbalance"]
     prefix  = args["output"]
     expo    = args["exposure"]
-
+    rot     = args["rotation"]
 
     cap = cv2.VideoCapture(input)
     frame = None
@@ -60,11 +62,12 @@ if __name__ == "__main__":
     #
     # compute normalization matrix from wframe and wbal and exposure
     #
+    print('n0',n0,'n1',n1,'skip',skip)
     W = None
     while (cap.isOpened()):
         if frame is None:
             ret, frame = cap.read()
-            x = np.array(frame)
+            x = np.array(frame,dtype=float)
             wmax = np.max(wbal)
             #print('wmax',wmax)
             W = np.zeros(x.shape)
@@ -75,19 +78,23 @@ if __name__ == "__main__":
         else:
             ret, frame = cap.read(frame)
         if not ret:
+            print('end of stream at frame',n)
             break
         
         n += 1
         
-        if n < n0 or n % skip:
+        if (n < n0) or (n % skip):
             continue
-        
+
         if n >= n1:
             break
-
         x = np.flip(np.array(frame),axis=2)
-        x_adj = np.minimum(255,np.maximum(0,(x * W)))
-        x_adj = x_adj.astype(np.uint8)
+        x_adj = x * W
+        x_under = np.sum(x_adj < 0)
+        x_over  = np.sum(x_adj > 255)
+        if x_under or x_over:
+            print('WARNING: under/over exposed pixels!: underexposed:',x_under,'overexposed:',x_over)
+        x_adj = np.minimum(255,np.maximum(0,x_adj))
         fps = (n-n0)/(time.time()-t0)
         print(f'frame {n:05d}  fps {fps:7.1f}')
         #plt.figure()
@@ -96,6 +103,13 @@ if __name__ == "__main__":
         #plt.subplot(2,1,2)
         #plt.imshow(x_adj)
         #plt.show()
+        if rot:
+            x = trans.rotate(x,-rot,resize=True)
+            x_adj = trans.rotate(x_adj,-rot,resize=True)
+
+        x     = (255*x).astype(np.uint8)
+        x_adj = x_adj.astype(np.uint8)
+
         imgio.imsave(f'{prefix}_frame_{n:05d}_orig.png',x)
         imgio.imsave(f'{prefix}_frame_{n:05d}_adj.png',x_adj)
     cap.release()
