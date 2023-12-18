68767faa9136ce66127fc246c767ef4fe4c721ce
Ignacio Ramírez Paulino <nacho@fing.edu.uy>
2023-12-18 12:43:15 -0300

    tuned calibrate pair script

--- scripts/calib3d_calibrate_pair.sh
+++ scripts/calib3d_calibrate_pair.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-./calibrate_pair.py -i ../data/calibracion/checkers -l ../data/calibracion/checkers/frames_gopro1.list -r ../data/calibracion/checkers/frames_gopro2.list -o tmp -n 40 --downscale 2
+mkdir -p results/calib3d/pair
+code/calibrate_pair.py -i data/calibracion/checkers -l data/calibracion/checkers/frames_gopro1.list -r data/calibracion/checkers/frames_gopro2.list -o results/calib3d/pair --downscale 2 --pattern-size 41.2
 
