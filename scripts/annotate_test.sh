9d062c51709acbb881e9b73801e7003f7c91c715
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2024-03-25 09:46:37 -0300

    added data begin and end

---
+++ scripts/annotate_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./code/annotate_video.py \
+	--input-one ./datos/2023-12-11/gopro1/gopro1_white_3_y_toma_3.mp4 \
+	--input-two ./datos/2023-12-11/gopro2/gopro2_white_3_y_toma_3.mp4 \
+	--outfile results/annotate_test.json
