8af1003c20a4bafd36a8b803825f31f64a990d36
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2024-12-03 14:51:07 -0300

    sdfs

--- scripts/sandbox/calib3d_process_frames.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir results/calib3d
-for i in 1 2
-do
-  mkdir -p results/calib3d/gopro${i}
-  code/calibrate_camera.py -l data/calibracion/checkers/frames_gopro${i}.list -i data/calibracion/checkers -o results/calib3d/gopro${i}  --nframes 50 --downscale 2 --pattern-size 41.2
-done
